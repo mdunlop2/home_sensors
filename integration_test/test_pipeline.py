a945d17e03d5cf4a8da7ca3d79caa7c352e3f906
Matthew Dunlop <matthewdunlop22@gmail.com>
2024-05-06 14:26:54 +0100

    tidy up integration test

--- integration_test/test_pipeline.py
+++ integration_test/test_pipeline.py
@@ -5,9 +5,10 @@ from lib.data.raw import download_raw_data_if_not_exists
 from lib.data.split import add_train_valid_test_split_table
 
 
-def test_full_pipeline() -> None:
+def test_integration() -> None:
     """
-    Run through the entire pipeline used in the notebook series to ensure it functions correctly
+    Covers the download of the raw sensor data and full feature engineering pipeline
+    as used in notebooks.
     """
     with tempfile.NamedTemporaryFile(suffix=".db") as temp_db_file:
         download_raw_data_if_not_exists(temp_db_file.name)
@@ -17,9 +18,3 @@ def test_full_pipeline() -> None:
         multi_location_windows = ["5min", "30min", "1h", "2h"]
         df = read_raw_data(temp_db_file.name, train=True)
         _ = add_all_features(df, multi_location_windows)
-
-        # transformation
-
-        # model fit
-
-        # model validations
